009c649a4ca3d591ffa6b37c0fa524323f6d8920
Daniel <dleal.pizarro97@gmail.com>
2024-05-05 14:18:15 +0200

    Cambios en los agentes del jenkinsfile

--- jenkins/JENKINSFILE_agentes.groovy
+++ jenkins/JENKINSFILE_agentes.groovy
@@ -1,39 +1,59 @@
 pipeline {
-    agent any
+    agent none
 
     stages {
         stage('Get Code') {
+            //agent { node 'agent-aux' }
+            agent any
             steps {
-                //git 'https://github.com/anieto-unir/helloworld.git'
                 git 'https://github.com/dleal97/unir-helloworld.git'
+                stash name: 'code', includes: '**'
+                echo 'Código guardado en el stash.'
+                echo "${env.NODE_NAME}"
             }
         }
         
         stage('Build') {
+            //agent { node 'agent-aux' }
+            agent any
             steps {
+                unstash 'code'
                 echo 'Esto es Python, no hay que compilar nada!!'
                 echo WORKSPACE
-                bat 'dir'
+                bat '''
+                    whoami
+                    hostname
+                    dir
+                '''
             }
         }
         
         stage('Test') {
             parallel {
                 stage('Unit') {
+                    agent { node 'agent-aux2' }
                     steps {
                         catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
-                           bat '''
+                            unstash 'code'
+                            echo "${env.NODE_NAME}" 
+                            bat '''
+                                whoami
+                                hostname
                                 set PYTHONPATH=%WORKSPACE%
                                 \\Users\\dleal\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pytest.exe --junitxml=result-unit.xml test/unit
-                            '''                    
+                            '''
                         }
                     }
                 }
         
                 stage('Rest') {
+                    agent { node 'agent-aux3' }
                     steps {
                         catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
-                           bat '''
+                            unstash 'code'
+                            bat '''
+                                whoami
+                                hostname
                                 set FLASK_APP=app\\api.py
                                 start \\Users\\dleal\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\flask.exe run
                                 start java -jar \\Users\\dleal\\Documents\\Kschool\\CURSO\\Wiremock\\wiremock-standalone-3.5.4 -port 9090 --root-dir test/wiremock
@@ -48,10 +68,18 @@ pipeline {
         }
         
         stage('Results') {
+            agent { node 'agent-aux' }
             steps {
+                unstash 'code'
                 junit 'result*.xml'
                 echo 'FINISH!!'
             }
+        }
+    }
+    
+    post {
+        always {
+            clearWs()
         }
     }
 }
