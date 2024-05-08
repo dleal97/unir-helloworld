8b2e98a3361dcbd39bb4af4ec5d95d823990fb05
dleal97 <111768917+dleal97@users.noreply.github.com>
2024-05-08 19:10:46 +0200

    Corregido mensaje assertEqual para substract y divide

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -32,7 +32,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "1", "ERROR ADD"
+            response.read().decode(), "1", "ERROR SUBSTRACT"
         )
 
     def test_api_divide(self):
@@ -42,7 +42,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "3.0", "ERROR ADD"
+            response.read().decode(), "3.0", "ERROR DIVIDE"
         )
 
     def test_api_sqrt(self):
