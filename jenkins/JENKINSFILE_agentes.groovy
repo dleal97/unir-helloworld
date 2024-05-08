2e5e9f8b8946f082cf8650a44f9e0c0c7fe082ec
Daniel <dleal.pizarro97@gmail.com>
2024-05-08 18:56:10 +0200

    Timeout corregido

--- jenkins/JENKINSFILE_agentes.groovy
+++ jenkins/JENKINSFILE_agentes.groovy
@@ -42,6 +42,7 @@ pipeline {
                                 set PYTHONPATH=%WORKSPACE%
                                 \\Users\\dleal\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pytest.exe --junitxml=result-unit.xml test/unit
                             '''
+                            stash name:'unit-res', includes:'result-unit.xml'
                         }
                     }
                 }
@@ -57,10 +58,37 @@ pipeline {
                                 set FLASK_APP=app\\api.py
                                 start \\Users\\dleal\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\flask.exe run
                                 start java -jar \\Users\\dleal\\Documents\\Kschool\\CURSO\\Wiremock\\wiremock-standalone-3.5.4 -port 9090 --root-dir test/wiremock
-                                
+                            '''
+                            script {
+                                def urls = ['http://localhost:5000', 'http://localhost:9090']
+                                boolean connected = false
+
+                                while (!connected) {
+                                    for (url in urls) {
+                                        try {
+                                            def response = sh(script: "curl -s -o /dev/null -w '%{http_code}' ${url}", returnStdout: true).trim()
+                                            if (response == '200') {
+                                                println "Conexión establecida con ${url}"
+                                                connected = true
+                                            } else {
+                                                println "No se pudo establecer conexión con ${url}. Código de estado HTTP: ${response}"
+                                            }
+                                        } catch (Exception e) {
+                                            println "Error al intentar conectar con ${url}: ${e.message}"
+                                        }
+                                    }
+                                    if (!connected) {
+                                        println "Intentando nuevamente en 10 segundos..."
+                                        sleep(time: 10, unit: 'SECONDS')
+                                    }
+                                }
+                            }
+                                                      
+                            bat '''   
                                 set PYTHONPATH=.
                                 \\Users\\dleal\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pytest.exe --junitxml=result-rest.xml test/rest
-                            '''    
+                            ''' 
+                            stash name:'rest-res', includes:'result-res.xml'      
                         }
                     }
                 }               
@@ -68,9 +96,10 @@ pipeline {
         }
         
         stage('Results') {
-            agent { node 'agent-aux' }
+            agent any
             steps {
-                unstash 'code'
+                unstash name:'unit-res'
+                unstash name:'rest-res'
                 junit 'result*.xml'
                 echo 'FINISH!!'
             }
